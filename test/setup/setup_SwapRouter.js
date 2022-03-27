127ad5e362bdccf6b1ee4cc4e97e3f1d6c53b74f
swordc <swordc@126.com>
2022-03-27 16:06:12 +0800

    add testcase for router

--- test/setup/setup_SwapRouter.js
+++ test/setup/setup_SwapRouter.js
@@ -26,10 +26,10 @@ import {
 } from "./setup_SwapFactory"
 
 /**
- * SwapExactTokensForTokens
+ * SplitSwapExactTokensForTokens
  * @returns {Promise<*>}
  */
-export const swapExactTokensForTokens = async(
+export const splitSwapExactTokensForTokens = async(
     userAddr,
     Array_tokenNamePath,
     Array_amountInSplit,
@@ -80,10 +80,10 @@ export const swapExactTokensForTokens = async(
 }
 
 /**
- * SwapTokensForExactTokens
+ * SplitSwapTokensForExactTokens
  * @returns {Promise<*>}
  */
- export const swapTokensForExactTokens = async(
+ export const splitSwapTokensForExactTokens = async(
     userAddr,
     Array_tokenNamePath,
     Array_amountOutSplit,
@@ -128,6 +128,114 @@ export const swapExactTokensForTokens = async(
         tokenKeyPath,
         Array_amountOutSplit,
         amountInMax,
+        parseFloat(curTimestamp) + parseFloat(duration)
+    ]
+    return sendTransaction({ code, args, signers })
+}
+
+/**
+ * SwapExactTokensForTokens
+ * @returns {Promise<*>}
+ */
+ export const swapExactTokensForTokens = async(
+    userAddr,
+    Array_tokenNamePath,
+    amountIn,
+    amountOutMin,
+    duration,
+) => {
+    let tokenInName = Array_tokenNamePath[0]
+    let tokenOutName = Array_tokenNamePath[Array_tokenNamePath.length-1]
+    let tokenKeyPath = []
+    for (let i = 0; i < Array_tokenNamePath.length; ++i) {
+        let tokenName = Array_tokenNamePath[i]
+        tokenKeyPath.push(await getTokenKeyByName(tokenName))
+    }
+
+    let tokenInPathPrefix = await getTokenPathPrefixByName(tokenInName)
+    let tokenOutPathPrefix = await getTokenPathPrefixByName(tokenOutName)
+
+    let tokenInVaultPath = "/storage/" + tokenInPathPrefix + "Vault"
+    let tokenOutVaultPath = "/storage/" + tokenOutPathPrefix + "Vault"
+    let tokenOutReceiverPath = "/public/" + tokenOutPathPrefix + "Receiver"
+    let tokenOutBalancePath = "/public/" + tokenOutPathPrefix + "Balance"
+
+    const cadenceRootPath = "../../src";
+    const basePath = path.resolve(__dirname, cadenceRootPath);
+    var code = fs.readFileSync(basePath+"/transactions/user/swap_exact_tokens_for_tokens.template", "utf-8");
+    code = code.replace("tokenInVaultPath: StoragePath,", "")
+    code = code.replace("tokenOutVaultPath: StoragePath,", "")
+    code = code.replace("tokenOutReceiverPath: PublicPath,", "")
+    code = code.replace("tokenOutBalancePath: PublicPath,", "")
+    code = code.replaceAll("tokenInVaultPath", tokenInVaultPath)
+    code = code.replaceAll("tokenOutVaultPath", tokenOutVaultPath)
+    code = code.replaceAll("tokenOutReceiverPath", tokenOutReceiverPath)
+    code = code.replaceAll("tokenOutBalancePath", tokenOutBalancePath)
+
+    code = code.replaceAll("Token1Addr", await getTokenAddrByName(tokenOutName))
+    code = code.replaceAll("Token1Name", tokenOutName)
+
+    let curTimestamp = (await queryTimestamp())[0]
+
+    const signers = [userAddr]
+    const args = [
+        tokenKeyPath,
+        amountIn,
+        amountOutMin,
+        parseFloat(curTimestamp) + parseFloat(duration)
+    ]
+    return sendTransaction({ code, args, signers })
+}
+
+/**
+ * SplitSwapTokensForExactTokens
+ * @returns {Promise<*>}
+ */
+ export const swapTokensForExactTokens = async(
+    userAddr,
+    Array_tokenNamePath,
+    amountOut,
+    amountInMax,
+    duration,
+) => {
+    let tokenInName = Array_tokenNamePath[0]
+    let tokenOutName = Array_tokenNamePath[Array_tokenNamePath.length-1]
+    let tokenKeyPath = []
+    for (let i = 0; i < Array_tokenNamePath.length; ++i) {
+        let tokenName = Array_tokenNamePath[i]
+        tokenKeyPath.push(await getTokenKeyByName(tokenName))
+    }
+
+    let tokenInPathPrefix = await getTokenPathPrefixByName(tokenInName)
+    let tokenOutPathPrefix = await getTokenPathPrefixByName(tokenOutName)
+
+    let tokenInVaultPath = "/storage/" + tokenInPathPrefix + "Vault"
+    let tokenOutVaultPath = "/storage/" + tokenOutPathPrefix + "Vault"
+    let tokenOutReceiverPath = "/public/" + tokenOutPathPrefix + "Receiver"
+    let tokenOutBalancePath = "/public/" + tokenOutPathPrefix + "Balance"
+
+    const cadenceRootPath = "../../src";
+    const basePath = path.resolve(__dirname, cadenceRootPath);
+    var code = fs.readFileSync(basePath+"/transactions/user/swap_tokens_for_exact_tokens.template", "utf-8");
+    code = code.replace("tokenInVaultPath: StoragePath,", "")
+    code = code.replace("tokenOutVaultPath: StoragePath,", "")
+    code = code.replace("tokenOutReceiverPath: PublicPath,", "")
+    code = code.replace("tokenOutBalancePath: PublicPath,", "")
+    code = code.replaceAll("tokenInVaultPath", tokenInVaultPath)
+    code = code.replaceAll("tokenOutVaultPath", tokenOutVaultPath)
+    code = code.replaceAll("tokenOutReceiverPath", tokenOutReceiverPath)
+    code = code.replaceAll("tokenOutBalancePath", tokenOutBalancePath)
+
+    code = code.replaceAll("Token1Addr", await getTokenAddrByName(tokenOutName))
+    code = code.replaceAll("Token1Name", tokenOutName)
+
+    let curTimestamp = (await queryTimestamp())[0]
+
+    const signers = [userAddr]
+    const args = [
+        tokenKeyPath,
+        amountInMax,
+        amountOut,
         parseFloat(curTimestamp) + parseFloat(duration)
     ]
     return sendTransaction({ code, args, signers })
--- test/test/SwapRouter.test.js
+++ test/test/SwapRouter.test.js
@@ -35,6 +35,8 @@ import {
 } from "../setup/setup_SwapPair"
 
 import {
+    splitSwapExactTokensForTokens,
+    splitSwapTokensForExactTokens,
     swapExactTokensForTokens,
     swapTokensForExactTokens
 } from "../setup/setup_SwapRouter"
@@ -55,8 +57,8 @@ describe("Swap Pair Testsuites", () => {
     afterEach(async () => {
         return emulator.stop();
     });
-    
-    it("Swap Exact Tokens for Tokens - one swap", async () => {
+    /*
+    it("Split Swap Exact Tokens for Tokens - one swap", async () => {
         const liquidityProvider = await getAccountAddress("liquidityProvider");
         const trader = await getAccountAddress("trader");
 
@@ -81,7 +83,7 @@ describe("Swap Pair Testsuites", () => {
 
 
         // Swap 
-        let swapRes = await swapExactTokensForTokens(
+        let swapRes = await splitSwapExactTokensForTokens(
             trader,
             [tokenName0, tokenName1],
             ["10.0"],
@@ -111,7 +113,7 @@ describe("Swap Pair Testsuites", () => {
         )
     });
     
-    it("Swap Exact Tokens for Tokens - multi swap", async () => {
+    it("Split Swap Exact Tokens for Tokens - multi swap", async () => {
         const liquidityProvider = await getAccountAddress("liquidityProvider");
         const trader = await getAccountAddress("trader");
 
@@ -136,7 +138,7 @@ describe("Swap Pair Testsuites", () => {
         //console.log('local vault', (await getBalanceByName(tokenName0, trader))[0], (await getBalanceByName(tokenName1, trader))[0])
 
         // Swap 
-        let swapRes = await swapExactTokensForTokens(
+        let swapRes = await splitSwapExactTokensForTokens(
             trader,
             ["FUSD", "ETH", "FUSD", "USDC", "ETH"],
             ["5.0", "5.0"],
@@ -169,6 +171,109 @@ describe("Swap Pair Testsuites", () => {
     });
     
     
+    it("Split Swap Tokens for Exact Tokens - one swap", async () => {
+        const liquidityProvider = await getAccountAddress("liquidityProvider");
+        const trader = await getAccountAddress("trader");
+
+        let tokenName0 = "FUSD";
+        let tokenName1 = "USDC";
+        await deployTokenByName(tokenName0);
+        await deployTokenByName(tokenName1);
+
+        await mintTokenByName(tokenName0, liquidityProvider, "15000000.0");
+        await mintTokenByName(tokenName1, liquidityProvider, "15000000.0");
+        await mintTokenByName(tokenName0, trader, "15.0");
+
+        
+        await createPair(tokenName0, tokenName1);
+
+        const reserve0 = 14000000.0
+        const reserve1 = 9000000.0
+
+        await addLiquidity(liquidityProvider, tokenName0, tokenName1, reserve0, reserve1, 0.1, 500)
+        //console.log('init', await queryPairInfoByTokenName(tokenName0, tokenName1))
+        //console.log('local vault', (await getBalanceByName(tokenName0, trader))[0], (await getBalanceByName(tokenName1, trader))[0])
+
+
+        // Swap 
+        let swapRes = await splitSwapTokensForExactTokens(
+            trader,
+            [tokenName0, tokenName1],
+            ["5.12345678"],
+            "99999.0",
+            "500.0"
+        )
+        expect(swapRes[1]).toBeNull()
+
+        const afterPairInfo = (await queryPairInfoByTokenName(tokenName0, tokenName1))[0]
+        //console.log('init', await queryPairInfoByTokenName(tokenName0, tokenName1))
+        //console.log('local vault', (await getBalanceByName(tokenName0, trader))[0], (await getBalanceByName(tokenName1, trader))[0])
+        
+        let amountOut = 6.0
+        let amountIn = amountOut * reserve0 / (reserve1 - amountOut) / 0.997
+        //console.log('estimate in', amountIn)
+        //console.log(afterPairInfo)
+        
+        expect(
+            (await getBalanceByName(tokenName1, trader))[0]
+        ).toBe(
+            "5.12345678"
+        )
+    });
+    
+
+    it("Swap Exact Tokens for Tokens - multi swap", async () => {
+        const liquidityProvider = await getAccountAddress("liquidityProvider");
+        const trader = await getAccountAddress("trader");
+
+        await deployTokenByName("FUSD");
+        await deployTokenByName("USDC");
+        await deployTokenByName("ETH");
+
+        await mintTokenByName("FUSD", liquidityProvider, "10000000.0");
+        await mintTokenByName("USDC", liquidityProvider, "10000000.0");
+        await mintTokenByName("ETH", liquidityProvider, "10000000.0");
+
+        await mintTokenByName("FUSD", trader, "10.0");
+        
+        await createPair("FUSD", "USDC");
+        await createPair("FUSD", "ETH");
+        await createPair("USDC", "ETH");
+
+        await addLiquidity(liquidityProvider, "FUSD", "USDC", "1000", "1000", 0.1, 500)
+        await addLiquidity(liquidityProvider, "FUSD", "ETH", "1000", "1", 0.1, 500)
+        await addLiquidity(liquidityProvider, "USDC", "ETH", "1000", "1", 0.1, 500)
+        //console.log('init', await queryPairInfoByTokenName(tokenName0, tokenName1))
+        //console.log('local vault', (await getBalanceByName("FUSD", trader))[0], (await getBalanceByName("ETH", trader))[0])
+
+        // Swap 
+        let swapRes = await swapExactTokensForTokens(
+            trader,
+            ["FUSD", "USDC", "ETH"],
+            "5.0",
+            "0.0",
+            "500.0"
+        )
+        expect(swapRes[1]).toBeNull()
+
+        const afterPairInfo = (await queryPairInfoByTokenName("FUSD", "ETH"))[0]
+        //console.log('init', await queryPairInfoByTokenName(tokenName0, tokenName1))
+        //console.log('local vault', (await getBalanceByName("FUSD", trader))[0], (await getBalanceByName("ETH", trader))[0])
+        
+        let amountIn = 5.0
+        let amountOut1 = amountIn * 0.997 * 1.0 / (1000.0 + amountIn * 0.997)
+        let amountOut2_1 = amountIn * 0.997 * 1000.0 / (1000.0 + amountIn * 0.997)
+        let amountOut2_2 = amountOut2_1 * 0.997 * 1.0 / (1000.0 + amountOut2_1 * 0.997)
+        //console.log('estimate out', amountOut1+amountOut2_2)
+        
+        expect(
+            (await getBalanceByName("ETH", trader))[0]
+        ).toBe(
+            "0.00492105"
+        )
+    });
+    */
+
     it("Swap Tokens for Exact Tokens - one swap", async () => {
         const liquidityProvider = await getAccountAddress("liquidityProvider");
         const trader = await getAccountAddress("trader");
@@ -197,8 +302,8 @@ describe("Swap Pair Testsuites", () => {
         let swapRes = await swapTokensForExactTokens(
             trader,
             [tokenName0, tokenName1],
-            ["5.12345678"],
-            "99999.0",
+            "5.12345678",
+            "10.0",
             "500.0"
         )
         expect(swapRes[1]).toBeNull()
